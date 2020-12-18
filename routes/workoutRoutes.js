acd645ceec3660feca14cf501b8f134b637e989f
Richard Shaw <richardmorganshaw@gmail.com>
2020-12-18 10:50:07 -0800

    Routes put and post

---
+++ routes/workoutRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+const { Workout } = require('../models')
+
+router.post('/workouts', (req, res) => {
+  Workout.create({})
+    .then(workout => res.json(workout))
+    .catch(err => console.log(err))
+})
+
+router.put('/workouts/:id', (req, res) => {
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { tasks: req.body } }),
+    { new: true, runValidators: true })
+  .then(workout => res.json(workout))
+  .catch(err => console.log(err))
+})
